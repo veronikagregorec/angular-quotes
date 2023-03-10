8332f23a9a0cf918cc879409e1eaacb65f2c6777
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-10 14:19:40 +0100

    Added quotes

--- src/app/posts/service/http.service.ts
+++ src/app/posts/service/http.service.ts
@@ -6,9 +6,11 @@ import { PostInterface } from '../types/post.interface';
 export class PostsService {
   getPosts(): Observable<PostInterface[]> {
     const posts = [
-      { id: '1', title: 'First post' },
-      { id: '2', title: 'Second post' },
-      { id: '3', title: 'Third post' },
+      { id: '1', title: '"Be yourself; everyone else is already taken." - Oscar Wilde' },
+      { id: '2', title: '"So many books, so little time." - Frank Zappa'},
+      { id: '3', title: '"You only live once, but if you do it right, once is enough." - Mae West' },
+      { id: '4', title: '"Be the change that you wish to see in the world." - Mahatma Gandhi' },
+      { id: '5', title: '"A friend is someone who knows all about you and still loves you." - Elbert Hubbard ' },
     ];
     return of(posts).pipe(delay(2000));
   }
