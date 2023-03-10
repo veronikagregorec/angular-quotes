5e2a619ea857e43cdf367ec2344c910baeeb2581
veronikagregorec <gregorec.veronika@gmail.com>
2023-03-10 14:17:35 +0100

    Made posts modul

--- src/app/posts/posts/posts.module.ts
+++ src/app/posts/posts/posts.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsComponent } from '../posts/posts.component';
-import { StoreModule } from '@ngrx/store/src';
+import { StoreModule } from '@ngrx/store';
 import { reducers } from '../store/reducers';
 import { PostsService } from '../service/http.service';
+import { EffectsModule } from '@ngrx/effects';
+import { PostsEffects } from '../store/effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forFeature('posts', reducers)
+    StoreModule.forFeature('posts', reducers),
+    EffectsModule.forFeature([PostsEffects])
   ],
   providers: [PostsService],
   declarations: [PostsComponent],
